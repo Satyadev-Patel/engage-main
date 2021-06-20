20d8885b17cd2976efea9f95bceafcf82ace0004
Satyadev-Patel <satyadev300302@gmail.com>
2021-06-21 03:36:41 +0530

    ready to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,10 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import { useState } from "react"
 import Login from "./components/Login"
-import Home from "./components/Home"
 import Meeting from "./components/Meeting"
 import Header from "./components/Header"
-import { makeStyles } from "@material-ui/core/styles"
 import { CssBaseline } from "@material-ui/core"
-import io from "socket.io-client";
-import Googlelogin from "./components/GoogleLogin"
 
-const socket = io.connect("http://localhost:5000");
 
 function App() {
   const [Auth, setAuth] = useState(
@@ -29,7 +24,7 @@ function App() {
             {Auth === "Yes" ? <Header Auth = {Auth} Authenticate={Authenticate}/> 
             : <Login Auth = {Auth} Authenticate={Authenticate} />}
           </Route>
-          <Route exact path = '/meeting'>
+          <Route path = '/meeting'>
             {Auth === "Yes" ? <Meeting Auth = {Auth} Authenticate={Authenticate}/>
             : <Login Auth = {Auth} Authenticate={Authenticate} />}
           </Route>
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 const io = require("socket.io")(server,{
     cors: {
+        origin: "http://localhost:3000",
         methods: ["GET","POST"]
     }
 })
