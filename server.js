014f733b1a57ff70e9cdb1e0a74f0af39ae88387
Satyadev-Patel <satyadev300302@gmail.com>
2021-06-22 00:25:13 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ if( process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname,'client/build/index.html'));
     });
 }
-
+app.get("/hello",(req,res)=>{
+    res.send("Success");
+})
 const port = process.env.PORT || 5000
 server.listen(port, () => console.log(`server is running on port ${port}`));
